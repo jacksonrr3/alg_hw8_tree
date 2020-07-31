7b2ea1b6e5a0b1ae31231c549741bf3bdd3f4410
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-07-31 13:15:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,33 @@
 #include "binary_search_tree.h"
 #include "decart_tree.h"
 #include "test.h"
-
+#include "RB_tree.h"
 
 
 
 int main() {
-
+//тесты BST
 	std::srand(std::time(nullptr));
 
 	test_bst_time<int>(2500);
 	test_dec_time<int>(2500);
 	
+//тест RB_tree	
+	RB_Tree<int> test_tree;
+
+	test_tree.insert(5);
+	test_tree.insert(6);
+	test_tree.insert(3);
+	test_tree.insert(9);
+	test_tree.insert(10);
+	test_tree.insert(1);
+	test_tree.insert(4);
+	test_tree.insert(7);
+	test_tree.insert(2);
+
+	test_tree.search(2);
+
+	test_tree.print_tree();
 
 	return 0;
 }
